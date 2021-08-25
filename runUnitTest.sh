d354fa6106202813677d97dbc48e9f536fcea678
Shane Unruh <shane.unruh@hpe.com>
2021-08-25 11:59:00 -0600

    Changed runUnitTest.sh to create an image with a unique tag

--- runUnitTest.sh
+++ runUnitTest.sh
@@ -23,4 +23,13 @@
 
 set -ex
 
-docker build $NO_CACHE -f Dockerfile.testing .
+GITSHA=$(git rev-parse HEAD)
+TIMESTAMP=$(date +"%Y-%m-%dT%H-%M-%SZ")
+IMAGE="cray/hms-bss-coverage"
+# image names must be lower case
+UNIQUE_TAG=$(echo ${IMAGE}_${GITSHA}_${TIMESTAMP} | tr '[:upper:]' '[:lower:]')
+# export NO_CACHE=--no-cache # this will cause docker build to run with no cache; off by default for local builds, enabled in jenkinsfile
+
+DOCKER_BUILDKIT=0 docker build $NO_CACHE -t $UNIQUE_TAG -f Dockerfile.testing .
+docker image rm $UNIQUE_TAG --force
+
