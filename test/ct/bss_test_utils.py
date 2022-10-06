eb7724b7b576b0e0e9b264d5443caf555661cb61
Ryan Sjostrand <sjostrand@hpe.com>
2022-10-06 11:58:35 -0500

    Checkin

--- test/ct/bss_test_utils.py
+++ test/ct/bss_test_utils.py
@@ -1,6 +1,19 @@
 import socket
 from box import Box
 
+import json
+import jmespath
+import logging
+import re
+
+from tavern.util import exceptions
+from tavern.util.dict_util import recurse_access_key,check_keys_match_recursive
+
+import yaml
+
+logger = logging.getLogger(__name__)
+
+
 def build_hsm_ethernet_interface_for_test_container():
     # Determine the IP of the container
     hostname = socket.gethostname()
@@ -22,3 +35,20 @@ def save_ip_address_of_test_container(response):
     ip_address = socket.gethostbyname(hostname)
 
     return Box({"test_container_ip_address": ip_address})
+
+def better_validate_regex(response, expression):
+    logger.debug("Matching %s with %s", response.text, expression)
+
+    match = re.search(expression, response.text)
+    if match is None:
+        raise exceptions.RegexAccessError(f"No match for regex: {expression}")
+
+    return {"regex": Box(match.groupdict())}
+
+
+def validate_yaml_simple(response, expected):
+    data = yaml.safe_load(response)
+
+    assert check_keys_match_recursive(expected, expected, [])
+
+    return {}
