781093caacb2bd80f9fa8a235cde7f4aca37bf4a
Ryan Sjostrand <sjostrand@hpe.com>
2022-10-18 11:22:57 -0500

    Add missing license

--- test/ct/bss_test_utils.py
+++ test/ct/bss_test_utils.py
@@ -1,3 +1,25 @@
+# MIT License
+#
+# (C) Copyright [2022] Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
 import socket
 from box import Box
 
@@ -12,7 +34,6 @@ from tavern.util.dict_util import recurse_access_key,check_keys_match_recursive
 import yaml
 
 logger = logging.getLogger(__name__)
-
 
 def build_hsm_ethernet_interface_for_test_container():
     # Determine the IP of the container
