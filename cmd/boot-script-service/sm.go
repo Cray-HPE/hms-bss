0ff22aa5a792b0493aab7b7483bcbfee61a25587
Joshua Williams <102987390+jwlv@users.noreply.github.com>
2025-02-24 14:37:26 -0800

    CASMHMS-6386: Off by one bug

--- cmd/boot-script-service/sm.go
+++ cmd/boot-script-service/sm.go
@@ -389,7 +389,7 @@ func protectedGetState(ts int64) (*SMData, map[string]SMComponent) {
 		if ts <= 0 {
 			smTimeStamp = currTime
 	    debugf("protectedGetState(): JW_DEBUG: setting smTimestamp to currTime")
-		} else if currTime - ts > cacheEvictionTimeout {
+		} else if currTime - ts >= cacheEvictionTimeout {
 			smTimeStamp = ts + cacheEvictionTimeout
 	    debugf("protectedGetState(): JW_DEBUG: setting smTimestamp to ts=%v + cacheEvictionTimeout=%v",
              time.Unix(ts, 0).Format("15:04:05"), cacheEvictionTimeout)
